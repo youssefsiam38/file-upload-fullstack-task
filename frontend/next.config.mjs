ae918b482104f9d41da8556956fd9a834d7f539f
youssefsiam38 <youssefsiam38@gmail.com>
2025-02-23 01:40:34 +0200

    configure nextjs as a proxy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@refinedev/antd"],
-  output: "standalone",
+    transpilePackages: ["@refinedev/antd"],
+    output: "standalone",
+    rewrites: async () => {
+        return {
+            beforeFiles: [
+                {
+                    source: "/api/:path*",
+                    destination: `${process.env.BACKEND_INTERNAL_URL}/api/:path*`,
+                },
+            ],
+        };
+    },
 };
 
 export default nextConfig;
